bbdc54791b02005c6fbe2a62bf1ef90d46532cc3
Gabin LONGONI <itsgabin@gmail.com>
2018-04-11 12:18:45 +0200

    Bouton RAZ fonctionnel dans l'interface Client

--- Gestion1/Vues/Menu/Client/Client.xaml.cs
+++ Gestion1/Vues/Menu/Client/Client.xaml.cs
@@ -19,29 +19,61 @@ namespace Gestion1.Vues.Menu.Client
 
         #region Chaine de connexion à la base de données SQL Server
 
-        private const string ConString = "Server=den1.mssql6.gear.host;Database=stock4;Uid=stock4;Pwd=gestionstock*"; // Chaine de connexion à la base SQL Server
+        private const string
+            ConString =
+                "Server=den1.mssql6.gear.host;Database=stock4;Uid=stock4;Pwd=gestionstock*"; // Chaine de connexion à la base SQL Server
 
         #endregion
 
-        #region Bouton Gestion du tableau Client
+        #region Gestion formulaire
+
+        private void DataGridClient_OnSelectionChanged(object sender, SelectionChangedEventArgs e) // Affichage des données de la table Clients dans les Textbox
+        {
+            DataGrid gd = (DataGrid)sender;
+            DataRowView rowSelected = gd.SelectedItem as DataRowView;
+            if (rowSelected != null)
+            {
+                TextBoxNom.Text = rowSelected["Nom"].ToString();
+                TextBoxPrenom.Text = rowSelected["Prenom"].ToString();
+                TextBoxSociete.Text = rowSelected["Societe"].ToString();
+                TextBoxTelephone.Text = rowSelected["Telephone"].ToString();
+                TextBoxEmail.Text = rowSelected["Email"].ToString();
+            }
+        }
+
+        #endregion
+
+        #region Boutons
 
         private void ButtonAjouter_OnClick(object sender, RoutedEventArgs e)
         {
             if (TextBoxNom.Text != "" & TextBoxPrenom.Text != "" & TextBoxSociete.Text != "" &
-                TextBoxTelephone.Text != "" & TextBoxEmail.Text != "") // Si les champs ne sont pas vides, la création est impossible
+                TextBoxTelephone.Text != "" &
+                TextBoxEmail.Text != "") // Si les champs ne sont pas vides, la création est impossible
             {
                 SqlConnection connection = new SqlConnection(ConString);
                 connection.Open(); // Ouvertue de la connexion
 
-                SqlCommand cmdSqlCommand = new SqlCommand("INSERT INTO dbo.Clients(Nom,Prenom,Societe,Telephone,Email) Values(@Nom, @Prenom, @Societe, @Telephone, @Email)", connection); // Requête d'insertion d'un nouveau client
+                SqlCommand cmdSqlCommand =
+                    new SqlCommand(
+                        "INSERT INTO dbo.Clients(Nom,Prenom,Societe,Telephone,Email) Values(@Nom, @Prenom, @Societe, @Telephone, @Email)",
+                        connection); // Requête d'insertion d'un nouveau client
 
                 cmdSqlCommand.Parameters.AddWithValue("@Nom", TextBoxNom.Text); // Paramètre du Nom du client
                 cmdSqlCommand.Parameters.AddWithValue("@Prenom", TextBoxPrenom.Text); // Paramètre du Prenom du client
-                cmdSqlCommand.Parameters.AddWithValue("@Societe", TextBoxSociete.Text); // Paramètre de la Societe du client
-                cmdSqlCommand.Parameters.AddWithValue("@Telephone", TextBoxTelephone.Text); // Paramètre du numéro de Telephone du client
+                cmdSqlCommand.Parameters.AddWithValue("@Societe",
+                    TextBoxSociete.Text); // Paramètre de la Societe du client
+                cmdSqlCommand.Parameters.AddWithValue("@Telephone",
+                    TextBoxTelephone.Text); // Paramètre du numéro de Telephone du client
                 cmdSqlCommand.Parameters.AddWithValue("@Email", TextBoxEmail.Text); // Paramètre de l'Email du client
                 cmdSqlCommand.ExecuteNonQuery(); // Execution de la requête
                 MessageBox.Show(""); // Affichage du message après execution de la requête
+                FillDataGrid(); // Recharge la table Clients
+                TextBoxNom.Clear(); // Vide le champs Nom
+                TextBoxPrenom.Clear(); // Vide le champs Prenom
+                TextBoxSociete.Clear(); // Vide le champs Societe
+                TextBoxTelephone.Clear(); // Vide le champs Telephone
+                TextBoxEmail.Clear(); // Vide le champs Email
                 connection.Close(); // Fermeture de la connexion
             }
         }
@@ -56,6 +88,15 @@ namespace Gestion1.Vues.Menu.Client
             throw new NotImplementedException();
         }
 
+        private void ButtonRaz_OnClick(object sender, RoutedEventArgs e)
+        {
+            TextBoxNom.Clear(); // Vide le champs Nom
+            TextBoxPrenom.Clear(); // Vide le champs Prenom
+            TextBoxSociete.Clear(); // Vide le champs Societe
+            TextBoxTelephone.Clear(); // Vide le champs Telephone
+            TextBoxEmail.Clear(); // Vide le champs Email
+        }
+
         #endregion
 
         #region Remplissage tableau Client
@@ -65,7 +106,8 @@ namespace Gestion1.Vues.Menu.Client
             string CmdString = string.Empty;
             using (SqlConnection con = new SqlConnection(ConString))
             {
-                CmdString = "SELECT Nom, Prenom, Societe, Telephone, Email FROM dbo.Clients"; // Requête de récupération des éléments de la table Clients
+                CmdString =
+                    "SELECT Nom, Prenom, Societe, Telephone, Email FROM dbo.Clients"; // Requête de récupération des éléments de la table Clients
                 SqlCommand cmd = new SqlCommand(CmdString, con);
                 SqlDataAdapter sda = new SqlDataAdapter(cmd);
                 DataTable dt = new DataTable("Clients");
@@ -73,9 +115,12 @@ namespace Gestion1.Vues.Menu.Client
                 DataGridClient.ItemsSource = dt.DefaultView; // Choix du type de vue sur l'interface graphique
             }
         }
+
         #endregion
 
-        private void TextBoxRecherche_OnTextChanged(object sender, TextChangedEventArgs e)
+        #region Gestion tableau Client
+
+        private void TextBoxRecherche_OnTextChanged(object sender, TextChangedEventArgs e) // Barre de recherche
         {
             if (ComboBoxCategorie.Text == "Nom")
             {
@@ -132,5 +177,8 @@ namespace Gestion1.Vues.Menu.Client
                 DataGridClient.ItemsSource = dt.DefaultView;
             }
         }
+
+        #endregion
+
     }
 }
